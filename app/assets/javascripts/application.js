57d813360f8a46a6b1d87f349d68618fbf22c00f
maruyu31 <you3746336@gmail.com>
2021-10-23 12:00:48 +0000

    [Add] フォーム追加を修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,25 +17,13 @@
 //= require_tree .
 /*global $*/
 
-var minCount = 1;
-var maxCount = 6;
-
-$(function(){
-  $('#demo-plus').on('click', function(){
-    var inputCount = $('#demo-area .unit').length;
-    if (inputCount < maxCount){
-      var element = $('#demo-area .unit:last-child').clone(true);
-      var inputList = element[0].querySelectorAll('input[type="text"]');
-      for (var i = 0; i < inputList.length; i++) {
-        inputList[i].value = "";
-      }
-      $('#demo-area .unit').parent().append(element);
-    }
-  });
-  $('.demo-minus').on('click', function(){
-    var inputCount = $('#demo-area .unit').length;
-    if (inputCount > minCount){
-      $(this).parents('.unit').remove();
-    }
-  });
-});
+var i = 1 ;
+function addForm() {
+  var input_data = document.createElement('input');
+  input_data.type = 'text';
+  input_data.id = 'inputform_' + i;
+  input_data.placeholder = 'フォーム-' + i;
+  var parent = document.getElementById('form_area');
+  parent.appendChild(input_data);
+  i++ ;
+}
